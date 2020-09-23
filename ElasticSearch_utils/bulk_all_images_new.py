a5f8bce224a01309d617033749c49453e08f4182
saeheejeon25@gmail.com <dlghk14!>
2020-09-23 14:57:26 +0900

    upload 20/09/23

--- ElasticSearch_utils/bulk_all_images_new.py
+++ ElasticSearch_utils/bulk_all_images_new.py
@@ -29,19 +29,18 @@ def get_all_info(prob_db):
     return df
 
 
-def preprocess(img_content, input_shape):
-    #     img = tf.io.read_file(img_path)
-    img = tf.image.decode_jpeg(img_content, channels=3, name="jpeg_reader")
+def preprocess_url(res, input_shape):
+
+    img = tf.image.decode_jpeg(res.content, channels=3, name="jpeg_reader")
     img = tf.image.resize(img, input_shape[:2])
     img = preprocess_input(img)
+
     return img
 
+# batch별로 데이터 elasticsearch에 넣음
+def bulk_batchwise(es, part_df, INDEX_NAME, INDEX_FILE):
 
-def get_fvec(url):
-    url = "https://s3.ap-northeast-2.amazonaws.com/mathflat" + url + "p.png"
-    url = url.replace("/math_problems/", "/math_problems/d/")
-
-    res = requests.get(url)
+    part_df.set_index("ID", inplace=True)
 
     input_shape = (224, 224, 3)
     base = tf.keras.applications.MobileNetV2(input_shape=input_shape,
@@ -49,14 +48,38 @@ def get_fvec(url):
                                              weights='imagenet')
     base.trainable = False
     model = Model(inputs=base.input, outputs=layers.GlobalAveragePooling2D()(base.output))
-    data = preprocess(res.content, input_shape)
-    print(data.shape)
-
-    return model.predict(data)
 
 
-# batch별로 image->feature vectors -> elasticsearch
-def bulk_batchwise(df, INDEX_FILE, INDEX_NAME):
+    id_list = []
+    res_list = []
+    for i in list(part_df.index):
+        url = "https://s3.ap-northeast-2.amazonaws.com/mathflat" + part_df.loc[i,'problemURL'] + "p.png"
+        url = url.replace("/math_problems/", "/math_problems/d/")
+        try:
+            res = requests.get(url)
+            id_list.append(i)
+            res_list.append(res)
+        except:
+            print("ID : ", i, "의 url이 유효하지 않습니다.")
+            pass
+
+    list_ds = tf.data.Dataset.from_tensor_slices(res_list)
+    ds = list_ds.map(lambda x: preprocess_url(x, input_shape), num_parallel_calls=-1)
+
+    dataset = ds.batch(batch_size).prefetch(-1)
+
+    for batch in dataset:
+        fvecs = model.predict(batch)
+
+    bulk(es, [{'_index': INDEX_NAME,
+                 'Id': f'{id_list[i]}', 'fvec': list(normalize(fvecs[i:i+1])[0].tolist()), 'unitCode' : f'{part_df.loc[id_list[i],'unitCode'}', 'problemLevel' : f'{part_df.loc[id_list[i],'problemLevel'}'}
+                for i in range(len(id_list))])
+
+    return
+
+
+# 모든 데이터를 넣음
+def bulk_all(df, INDEX_FILE, INDEX_NAME):
 
     es = Elasticsearch(hosts=['localhost:9200'])
     dim = 1280
@@ -72,18 +95,7 @@ def bulk_batchwise(df, INDEX_FILE, INDEX_NAME):
 
     for k in tqdm(range(nloop)):
 
-        rows = []
-
-        for i in range(k * bs, min((k + 1) * bs, df.shape[0])):
-
-            try:
-                fvecs = get_fvec(df.loc[i,'problemURL'])
-                rows.append({'_index': INDEX_NAME, 'Id': df.loc[i,'ID'], 'fvec': list(normalize(fvecs)[0].tolist()), 'unitCode': df.loc[i,'unitCode'], 'problemLevel':df.loc[i,'problemLevel']})
-            except:
-                pass
-        s = time.time()
-        bulk(es, rows)
-        print(k, time.time() - s)
+        bulk_batchwise(es, df.loc[k*bs:min((k+1)*bs, df.shape[0])],INDEX_NAME, INDEX_FILE)
 
     es.indices.refresh(index=INDEX_NAME)
     print(es.cat.indices(v=True))
@@ -102,6 +114,6 @@ if __name__=="__main__":
     INDEX_FILE = '../Test2/system/index2.json'
     INDEX_NAME = 'all_images'
 
-    bulk_batchwise(df, INDEX_FILE, INDEX_NAME)
+    bulk_all(df, INDEX_FILE, INDEX_NAME)
 
     print("Success!")
