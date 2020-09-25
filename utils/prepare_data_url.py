fc654fa6ffbebeabd79b0ad3f8031123b28f90a8
saeheejeon25@gmail.com <dlghk14!>
2020-09-25 14:58:21 +0900

    upload 20/09/25

--- utils/prepare_data_url.py
+++ utils/prepare_data_url.py
@@ -54,14 +54,16 @@ tf.compat.v1.enable_eager_execution()
 
 # Preprocess problem images to put them into MobileNet
 def preprocess(img_path, input_shape):
+
     img = tf.io.read_file(img_path)
     img = tf.image.decode_jpeg(img, channels=input_shape[2])
     img = tf.image.resize(img, input_shape[:2])
     img = preprocess_input(img)
+
     return img
 
 # Put images into pre-trained MobileNet to extract feature vectors
-def extract_feature(result_df, batch_size, input_shape, input_dir):
+def extract_feature(part_df, batch_size, input_shape):
 
     base = tf.keras.applications.MobileNetV2(input_shape=input_shape,
                                              include_top=False,
@@ -69,13 +71,34 @@ def extract_feature(result_df, batch_size, input_shape, input_dir):
     base.trainable = False
     model = Model(inputs=base.input, outputs=layers.GlobalAveragePooling2D()(base.output))
 
-    fnames = [input_dir +'/test'+str(Id)+'.png' for Id in list(result_df.index)]
-    list_ds = tf.data.Dataset.from_tensor_slices(fnames)
-    ds = list_ds.map(lambda x: preprocess(x, input_shape), num_parallel_calls=-1)
-    dataset = ds.batch(batch_size).prefetch(-1)
+    id_list = []
+    img_list = []
+    for i in list(part_df.index):
+        url = "https://s3.ap-northeast-2.amazonaws.com/mathflat" + part_df.loc[i,'problemURL'] + "p.png"
+        url = url.replace("/math_problems/", "/math_problems/d/")
+        try:
+            res = requests.get(url)
+
+            img_list.append(preprocess_from_url(res.content,input_shape))
+            id_list.append(i)
+        except:
+            print(f'ID : {i} 의 url이 유효하지 않습니다.')
+            pass
+
+    list_ds = tf.data.Dataset.from_tensor_slices(img_list)
+    # ds = list_ds.map(lambda x: preprocess_from_url(x, input_shape), num_parallel_calls=-1)
+
+    dataset = list_ds.batch(batch_size).prefetch(-1)
 
     for batch in dataset:
         fvecs = model.predict(batch)
+    # fnames = [input_dir +'/test'+str(Id)+'.png' for Id in list(result_df.index)]
+    # list_ds = tf.data.Dataset.from_tensor_slices(fnames)
+    # ds = list_ds.map(lambda x: preprocess(x, input_shape), num_parallel_calls=-1)
+    # dataset = ds.batch(batch_size).prefetch(-1)
+    #
+    # for batch in dataset:
+    #     fvecs = model.predict(batch)
 
     return fvecs
 
