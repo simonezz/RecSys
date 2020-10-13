0ccc3ffe80a287d4fae618aa5319e0a8d927b835
saeheejeon25@gmail.com <dlghk14!>
2020-10-13 17:45:41 +0900

    upload 20/10/13

--- server_test/twins_get_server.py
+++ server_test/twins_get_server.py
@@ -1,24 +1,41 @@
-# 데이터 이미 elasticsearch에 다 들어있다고 가정.
-# 아이디 입력 시 similar 문제 상위 10개 보여주는 것으로 함.
 import time
 
-# sys.path.insert(0, '../../utils')
 import general_utils as g_utils
+import pandas as pd
+import pymysql
 from elasticsearch import Elasticsearch
 
 
-class RecommenderSystem:
+class Twins_get_id:
 
     def __init__(self):
 
         self.ini_file = INI_FILE
         ini = g_utils.get_ini_parameters(self.ini_file)
-
-
         # Set parameters
-        self.es = ini['ElasticSearch']['host'] # Elasticsearch Host
+        self.es = ini['ElasticSearch']['host']  # Elasticsearch Host
         self.index_name = ini['ElasticSearch']['INDEX_NAME']
         self.search_size = ini['ElasticSearch']['SEARCH_SIZE']  # Similar problems 갯수 100으로 설정되어있음
+
+        # Connect to MySQL
+        self.user = ini['MySQL']['user']
+        self.passwd = ini['MySQL']['passwd']
+        self.host = ini['MySQL']['host']
+        self.db = ini['MySQL']['db']
+        self.charset = ini['MySQL']['charset']
+
+        self.prob_db = pymysql.connect(user=self.user, passwd=self.passwd, host=self.host, db=self.db,
+                                       charset=self.charset)
+
+    def find_id(self, book_id):
+
+        curs = self.prob_db.cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT ID, bookCode, bookDataID, twinsProblemID FROM iclass.BookTwins"
+        curs.execute(sql)
+        twins_df = pd.DataFrame(curs.fetchall())
+        id = twins_df.loc[twins_df['bookDataID'] == book_id, 'twinsProblemID']
+
+        return id
 
     def run(self, ID):
 
@@ -35,13 +52,13 @@ class RecommenderSystem:
             }
         )
 
-        if len(res['hits']['hits'])==0: #해당 ID가 Elasticsearch에 없다면,
+        if len(res['hits']['hits']) == 0:  # 해당 ID가 Elasticsearch에 없다면,
             raise Exception("해당 ID가 Elasticsearch에 없습니다!")
         for s in res['hits']['hits']:
             fvec = s['_source']['fvec']
             unitCode = s['_source']['unitCode']
             problemLevel = s['_source']['problemLevel']
-        print(f"문제 정보를 찾는데 걸리는 시간 : {(time.time()-search_info_time)} ms입니다.")
+            print(f"문제 정보를 찾는데 걸리는 시간 : {(time.time() - search_info_time)} ms입니다.")
 
         # 위에서 구한 unitCode, problemLevel의 문제들 중 코사인 유사도 구하기
 
@@ -74,11 +91,6 @@ class RecommenderSystem:
         ID_list = []
         for s in response['hits']['hits']:
 
-            # print("ID: ", s['_id'])
-            # print("unitCode: ", s['_source']['unitCode'])
-            # print("problemLevel: ", s['_source']['problemLevel'])
-            # print("score: ", s['_score'])
-            # print('---------------------')
             if s['_score'] < 2:
                 try:
                     if s['_id'] == ID_list[-1]:  # 중복
@@ -91,13 +103,12 @@ class RecommenderSystem:
         return ID_list
 
 
-INI_FILE = 'get_server.ini'
+INI_FILE = 'twins_get_server.ini'
 
 
-def find_similar_pb(ID):
-    ("찾고자 하는 ID: ", ID)
-    reco_system = RecommenderSystem()
+def find_similar_pb(book_id):
+    reco_system = Twins_get_id()
+    prob_id = int(reco_system.find_id(book_id))
+    # print(prob_id)
 
-    return reco_system.run(ID)
-
-
+    return prob_id, reco_system.run(prob_id)
