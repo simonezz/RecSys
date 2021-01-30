47f23513b84588f30c1b0f0d05b24122ac307f2c
Simone <73181519+simonezz@users.noreply.github.com>
2021-01-30 23:09:58 +0900

    Delete utils directory

--- utils/hml_equation_parser/__init__.py
+++
@@ -1,4 +0,0 @@
-from .hmlParser import convertEquation as convertEquationSample
-from .hmlParser import extract2HtmlStr as extract2HtmlStrSample
-from .hmlParser import parseHml as parseHmlSample
-from .hulkEqParser import hmlEquation2latex as eq2latex
